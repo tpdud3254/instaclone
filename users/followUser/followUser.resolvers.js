75f4fc14966abbb5f1385eb555eb0eb2e97ea40d
ksy <tpdud3256@gmail.com>
2022-10-06 23:47:46 +0900

    follow user

---
+++ users/followUser/followUser.resolvers.js
@@ -0,0 +1,36 @@
+import client from "../../client";
+import { protectedResolver } from "../user.utils";
+
+export default {
+    Mutation: {
+        followUser: protectedResolver(
+            async (__, { userName }, { loggedInUser }) => {
+                const ok = await client.user.findUnique({ where: userName });
+
+                if (!ok) {
+                    return {
+                        ok: false,
+                        error: "That user does not exist.",
+                    };
+                }
+
+                await client.user.update({
+                    where: {
+                        id: loggedInUser.id,
+                    },
+                    data: {
+                        following: {
+                            connect: {
+                                userName,
+                            },
+                        },
+                    },
+                });
+
+                return {
+                    ok: true,
+                };
+            }
+        ),
+    },
+};
